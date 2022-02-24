93e50b39488e9239c69adb9a5c011906f1cee204
Angelgroenen <bm.bracke.michael@gmail.com>
2022-02-24 17:02:02 +0100

    Last step case 3
    WIP

--- use-case-3/Student.php
+++ use-case-3/Student.php
@@ -22,3 +22,22 @@ class Student
     }
 }
 
+/* class StudentGroup
+{
+    public array $students;
+
+    public function __construct(array $students)
+        {
+            $this->students = $students;
+        }
+
+    public function getGroupInfo()
+    {
+        foreach ($this->students as $student){
+            echo "{$student->name} achieved a grade of {$student->grade}";
+        }
+    }
+}
+
+$group1 = new StudentGroup ([$student1, $student2, $student3, $student4, $student5, $student6, $student7, $student8, $student9, $student10]);
+$group1 = new StudentGroup ([$student11, $student12, $student3, $student4, $student5, $student6, $student7, $student8, $student9, $student10]); */
--- use-case-3/index.php
+++ use-case-3/index.php
@@ -62,5 +62,34 @@ $meanGroup2 = 0;
 }
 print_r(getGroupMean($students));
 
+
+
+
+
+
+
+// this function won't work because the foreach loop unpacks the array
+// but then on the line with the max function you are it expects an array again.
+// we could loop through the array, find who's part of what group
+// then push them into another array and then fnd the best and worst performing students
+function balanceGroupScores ($students){
+$bestStudent1 = "";
+$bestStudent2 = "";
+$worstStudent1 = "";
+$worstStudent2 = "";
+    foreach ($students as $student){
+        if ($student->group == 1) {
+            $bestStudent1 = max($students->grade);
+            $worstStudent1 = min($students->grade);
+        }
+        else if ($student->group == 2){
+            $bestStudent2 = max($students->grade);
+            $worstStudent2 = min($students->grade);
+        }
+    }    
+    return [$bestStudent1, $bestStudent2, $worstStudent1, $worstStudent2];   
+}
+print_r(balanceGroupScores($students));
+
 // je kan twee waardes returnen uit een functie met een array
 // return [value1, value2]
